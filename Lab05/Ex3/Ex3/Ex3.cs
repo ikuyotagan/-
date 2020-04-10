611b1be3d4d0306a2556c3d078fb1946e5c42ec1
ikuyotagan <63468028+ikuyotagan@users.noreply.github.com>
2020-04-10 20:18:11 +0300

    Add files via upload

---
+++ Lab05/Ex3/Ex3/Ex3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Ex3
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [] a = new int[n];
+            float sr = 0;
+            int sum = 0,
+                summin = 0,
+                sumplus = 0,
+                sumch = 0,
+                sumnech = 0,
+                max = -999999,
+                mini = -1,
+                min = 999999,
+                maxi = -1,
+                pro = 1;
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                sum += a[i];
+                sr = sum / n;
+                if (a[i] > 0) sumplus += a[i];
+                if (a[i] < 0) summin += a[i];
+                if (i % 2 == 0) sumch += a[i];
+                if (i % 2 == 1) sumnech += a[i];
+                if (max < a[i])
+                {
+                    max = a[i];
+                    maxi = i;
+                }
+
+                if (min > a[i])
+                {
+                    min = a[i];
+                    mini = i;
+                }
+            }
+
+            for (int i = Math.Min(mini,maxi) ; i < Math.Max(mini, maxi); i++)
+            {
+                pro *= a[i];
+            }
+            Console.WriteLine("Сумма элементов: {0}",sum);
+            Console.WriteLine("Сумма положительных элементов: {0}", sumplus);
+            Console.WriteLine("Сумма отрицательных элементов: {0}", summin);
+            Console.WriteLine("Среднее значение элемента: {0}", sr);
+            Console.WriteLine("Сумма элементов под четным номером: {0}", sumch);
+            Console.WriteLine("Сумма элементов под нечетным номером: {0}", sumnech);
+            Console.WriteLine("Максимальный элемент под номером {1} равен: {0}", max, maxi);
+            Console.WriteLine("Минимальный элемент под номером {1} равен: {0}", min, mini);
+            Console.WriteLine("Произведение элементов между ними: {0}", pro);
+        }
+    }
+}
---
+++ Lab05/MatrixMultiply/MatrixMultiply/MatrixMultiply.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MatrixMultiply
+{
+    class MatrixMultiply
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[2, 2];
+            Input(a);
+            int[,] b = new int[2, 2];
+            Input(b);
+            var result = Multiply(a, b);
+            Output(result);
+        }
+
+        private static void Input(int[,] a)
+        {
+            for (int r = 0; r < a.GetLength(0); r++)
+            {
+                for (int c = 0; c < a.GetLength(1); c++)
+                {
+                    Console.Write("Enter value for [{0},{1}] : ", r, c);
+                    string s = System.Console.ReadLine();
+                    a[r, c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static int[,] Multiply(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    result[r, c] += a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+            return result;
+        }
+
+        private static void Output(int[,] result)
+        {
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", result[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

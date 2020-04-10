a7b88da2a0973e497ecec5a30716a47b7e51c2f5
ikuyotagan <63468028+ikuyotagan@users.noreply.github.com>
2020-04-10 20:20:03 +0300

    Add files via upload

---
+++ Lab06/Ex3/Ex3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle(3,4,5);
+            Console.WriteLine(t1.Perimetr());
+            Console.WriteLine(t1.Ploschad());
+        }
+    }
+}
---
+++ Lab06/Ex3/Ex3/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex3
+{
+    class Triangle
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            if (a + c <= b || a + b <= c || b + c <= a)
+            {
+                throw new Exception();
+            }
+        }
+        public int Perimetr()
+        {
+            return this.c + this.b + this.a;
+        }
+
+        public double Ploschad()
+        {
+            int p = this.Perimetr() / 2;
+            return Math.Sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        }
+    }
+}

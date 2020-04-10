7625cbe538e70f429a530f97d81c4ed54d7a80dd
ikuyotagan <63468028+ikuyotagan@users.noreply.github.com>
2020-04-10 20:09:11 +0300

    Add files via upload

---
+++ Lab01/Divider/Divider/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Divider
+{
+    class DivideIt
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+                Console.WriteLine("Please enter the second integer");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+                int k = i / j;
+                Console.WriteLine("The result of dividing {0} by {1} is {2} ", i, j, k);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown: {0}", e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("An DivideByZero exception was thrown: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+        }
+    }
+}
---
+++ Lab01/Square/Square/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Square
+{
+    class Square
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите периметр");
+                string temp = Console.ReadLine();
+                int P = Int32.Parse(temp);
+                int a = P / 3;
+                int p = P / 2;
+                double S = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+                Console.WriteLine("{0:0.00}", S);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An wrong perimetr exception was thrown: {0}", e.Message);
+            }
+        }
+    }
+}

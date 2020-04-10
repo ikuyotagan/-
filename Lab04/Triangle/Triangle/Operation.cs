b0d92e0076637d288c748b66434175e618576ff6
ikuyotagan <63468028+ikuyotagan@users.noreply.github.com>
2020-04-10 20:16:15 +0300

    Add files via upload

---
+++ Lab04/Triangle/Triangle/Operation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Triangle
+{
+    class Operation
+    {
+        public static double Square(int a, int b, int c)
+        {
+            bool ok = Existance(a, b, c);
+            if (ok == false) 
+                return 0;
+            double p = (a + b + c)/2.0;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+
+        private static bool Existance(int a, int b, int c)
+        {
+            bool ok = true;
+            if ((a >= (b + c) || b >= (a + c)) || c >= (a + b)) ok = false;
+            return ok;
+        }
+        public static double Square(int a)
+        {
+            double p = 3 * a / 2.0;
+            double s = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+            return s;
+        }
+        public static int Root(int a, int b, int c, ref double x1, ref double x2)
+        {
+            int d = b * b - 4 * a * c;
+            if (d < 0) return -1;
+            if (d == 0)
+            {
+                x1 = (-b) / (2.0*a);
+                x2 = x1;
+                return 0;
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2.0 * a);
+            }
+            return 1;
+        }
+    }
+}
---
+++ Lab04/Triangle/Triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Console.WriteLine("Треугольник равносторонний(0/1)?");
+            int i = int.Parse(Console.ReadLine());
+            if (i == 0)
+            {
+                Console.WriteLine("Введите три стороны:");
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+                Console.WriteLine(Operation.Square(a, b, c));
+            }
+            else
+            {
+                Console.WriteLine("Введите сторону:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine(Operation.Square(a));
+            }
+            */
+            double x1=0, x2=0;
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int f = Operation.Root(a, b, c, ref x1, ref x2);
+            if (f == -1)
+            {
+                Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} нет.", a, b, c);
+            }
+            if (f == 0)
+            {
+                Console.WriteLine("Корень уравнения с коэффициентами a = {0}, b = {1}, c = {2} равен x1 = x2 = {3}", a, b, c, x1);
+            }
+            if (f == 1)
+            {
+                Console.WriteLine("Корни уравнения с коэффициентами a = {0}, b = {1}, c = {2} равны x1 = {3}, x2 = {4}.", a, b, c, x1, x2);
+            }
+        }
+    }
+}

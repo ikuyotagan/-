2079c4b0d55b7048f0e63d051011439f031c1a6a
ikuyotagan <63468028+ikuyotagan@users.noreply.github.com>
2020-04-10 20:12:11 +0300

    Add files via upload

---
+++ Lab02/Distance/Distance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Distance
+{
+    public struct Distance
+    {
+        public long feet;
+        public float inch;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance a, b, c;
+            Console.WriteLine("Введите футы первой переменной:");
+            a.feet = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите дюймы первой переменной:");
+            a.inch = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите футы второй переменной:");
+            b.feet = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите дюймы второй переменной:");
+            b.inch = float.Parse(Console.ReadLine());
+            c.feet = a.feet + b.feet + (int)(a.inch + b.inch) / 12;
+            c.inch = (a.inch + b.inch) % 12;
+            Console.WriteLine("Третья переменная равна {0} '- {1}\"", c.feet,c.inch);
+        }
+    }
+}

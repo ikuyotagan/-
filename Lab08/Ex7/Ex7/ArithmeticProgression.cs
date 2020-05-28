020e81fa7628b47eb055c1d756a8edd6bf6bd81f
ikuyotagan <63468028+ikuyotagan@users.noreply.github.com>
2020-05-28 11:25:00 +0300

    Add files via upload

---
+++ Lab08/Ex7/Ex7/ArithmeticProgression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex7
+{
+    class ArithmeticProgression : IProgression
+    {
+        public int a { get; set; }
+        public int d { get; set; }
+        public ArithmeticProgression(int a,int d)
+        {
+            this.a = a;
+            this.d = d;
+        }
+        public int GetElement(int k)
+        {
+            return a + d * (k - 1);
+        }
+    }
+}
---
+++ Lab08/Ex7/Ex7/IProgression.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex7
+{
+    interface IProgression
+    {
+        int GetElement(int k);
+    }
+}
